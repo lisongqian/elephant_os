1194b6e6ad982e991ee9d5b72dc636147a240221
lisongqian <mail@lisongqian.cn>
2022-10-20 15:56:19 +0800

    代码4-3
    1. 引入保护模式
    2. 更新脚本程序

--- mbr2disk.sh
+++ mbr2disk.sh
@@ -1,1 +1,4 @@
-dd if=./mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
+nasm -I include/ -o boot/mbr.bin mbr.S
+nasm -I include/ -o boot/loader.bin  loader.S
+dd if=boot/mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
+dd if=boot/loader.bin of=hd60M.img bs=512 count=4 seek=2 conv=notrunc
