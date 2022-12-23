8086ee165a59492e93503efc8a4edb10d26abff4
lisongqian <mail@lisongqian.cn>
2022-12-23 16:27:56 +0800

    mbr和loader生成路径更改

--- loader_compile.sh
+++ loader_compile.sh
@@ -1,2 +1,2 @@
-nasm -I boot/include/ -o boot/loader.bin  boot/loader.S
-dd if=boot/loader.bin of=hd60M.img bs=512 count=4 seek=2 conv=notrunc
+nasm -I boot/include/ -o build/loader.bin  boot/loader.S
+dd if=build/loader.bin of=hd60M.img bs=512 count=4 seek=2 conv=notrunc
